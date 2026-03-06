41c4f3c06e26b8967b75f4fb6fe9911a66568869
andrewdevweb <andrewdevweb@gmail.com>
2026-03-06 16:10:29 +0000

    diseño cybersec + header actualizado

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'CybSecStudy';
+export const SITE_DESCRIPTION = 'Apuntes, guías y recursos de ciberseguridad y redes.';
